0bda0462bca3e7dfdc7e8e4d5aad1f41242dd3b4
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-06-21 00:11:04 +0530

    using the given sandbox/main.go

--- sandbox/main.go
+++ sandbox/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/mrtyunjaygr8/http_server_golang/internal/database"
@@ -9,41 +10,91 @@ import (
 func main() {
 	c := database.NewClient("db.json")
 	err := c.EnsureDB()
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("database ensured!")
-	user, err := c.CreateUser("test2", "test", "test", 12)
+	user, err := c.CreateUser("test@example.com", "password", "john doe", 18)
 	if err != nil {
-		log.Println(err)
-	} else {
-		log.Println(user)
+		log.Fatal(err)
 	}
+	fmt.Println("user created", user)
 
-	newUser, err := c.UpdateUser("test2", "test1", "test1", 121)
-	log.Println(newUser)
+	updatedUser, err := c.UpdateUser("test@example.com", "password", "john doe", 18)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("user updated", updatedUser)
 
-	getUser, err := c.GetUser("test2")
+	gotUser, err := c.GetUser("test@example.com")
 	if err != nil {
-		log.Println(err)
-	} else {
-		log.Println(getUser)
+		log.Fatal(err)
 	}
-	getNotUser, err := c.GetUser("test21")
-	if err != nil {
-		log.Println(err)
-	} else {
-		log.Println(getNotUser)
-	}
+	fmt.Println("user got", gotUser)
 
-	err = c.DeleteUser("test2")
+	err = c.DeleteUser("test@example.com")
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
-	err = c.DeleteUser("test21")
+	fmt.Println("user deleted")
+
+	_, err = c.GetUser("test@example.com")
+	if err == nil {
+		log.Fatal("shouldn't be able to get user that was deleted")
+	}
+	fmt.Println("user confirmed deleted")
+
+	user, err = c.CreateUser("test@example.com", "password", "john doe", 18)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
+	fmt.Println("user recreated", user)
+
+	post, err := c.CreatePost("test@example.com", "my cat is way too fat")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("post created", post)
+
+	secondPost, err := c.CreatePost("test@example.com", "my cat is getting skinny now")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("another post created", secondPost)
+
+	posts, err := c.GetPosts("test@example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("got posts", posts)
+
+	err = c.DeletePost(post.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("deleted first post", posts)
+
+	posts, err = c.GetPosts("test@example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("got posts", posts)
+
+	err = c.DeletePost(secondPost.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("deleted second post", posts)
+
+	posts, err = c.GetPosts("test@example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("got posts", posts)
+
+	err = c.DeleteUser("test@example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("user redeleted")
 }
