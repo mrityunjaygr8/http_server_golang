930cdf8328e3cb2ba66e2c705d7a1859d5c44652
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-06-21 00:26:48 +0530

    error url test handler added

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/mrtyunjaygr8/http_server_golang/internal/database"
+	"github.com/mrtyunjaygr8/http_server_golang/utils"
 )
 
 func main() {
@@ -12,6 +15,7 @@ func main() {
 	const addr = "localhost:8080"
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/", testHandler)
+	serveMux.HandleFunc("/err", testErrorHandler)
 
 	srv := http.Server{
 		Handler:      serveMux,
@@ -25,7 +29,11 @@ func main() {
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte("{}"))
+	utils.RespondWithJSON(w, 200, database.User{
+		Email: "test@example.com",
+	})
+}
+
+func testErrorHandler(w http.ResponseWriter, r *http.Request) {
+	utils.RespondWithError(w, 404, fmt.Errorf("not found"))
 }
